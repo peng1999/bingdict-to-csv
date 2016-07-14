7f02642ba395d77a8ae8477964d196cfa52ee961
彭冠文 <pgw1999@outlook.com>
2016-07-14 23:03:11 +0800

    init commit

---
+++ BingDict2Csv/Program.fs
@@ -0,0 +1,14 @@
+﻿open FSharp.Data
+
+type MyVocabularyBook = XmlProvider< @"C:\Users\pgw19\OneDrive\Documents\BingDictionary\1000.xml">
+
+[<EntryPoint>]
+let main argv =
+    let vocabularyBook = MyVocabularyBook.Load(@"C:\Users\pgw19\OneDrive\Documents\BingDictionary\1000.xml")
+    let text =
+        vocabularyBook.Words.WordUnits
+        |> Array.map ((fun wordUnit -> wordUnit.HeadWord) >> string)
+        //|> String.concat System.Environment.NewLine
+    let writer = System.IO.File.WriteAllLines(@".\vocabularyBook.csv", text)
+    //
+    0 // return an integer exit code
