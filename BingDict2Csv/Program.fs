743b6558bfeaa3c2a53269bbe1245b6b39aa5aa6
彭冠文 <pgw1999@outlook.com>
2016-07-17 22:43:59 +0800

    使用命令行参数

--- BingDict2Csv/Program.fs
+++ BingDict2Csv/Program.fs
@@ -1,14 +1,29 @@
 ﻿open FSharp.Data
+open CommandLine
 
 type MyVocabularyBook = XmlProvider< @"xml-example.xml">
 
+type CommandOptions() = 
+    [<Option('d', "directory", Required = true)>]
+    member val VocabularyDir : string = ""
+    [<Option('o', "output", DefaultValue = ".")>]
+    member val OutputDir : string = "."
+
 [<EntryPoint>]
 let main argv =
+    let args = Parser.Default.ParseArguments<CommandOptions>(argv).Value
+    #if DEBUG
     let vocabularyBook = MyVocabularyBook.Load(@"C:\Users\pgw19\OneDrive\Documents\BingDictionary\1000.xml")
+    #else
+    let vocabularyBook = MyVocabularyBook.Load(args.VocabularyDir)
+    #endif
     let text =
         vocabularyBook.Words.WordUnits
         |> Array.map ((fun wordUnit -> wordUnit.HeadWord) >> string)
         //|> String.concat System.Environment.NewLine
+    #if DEBUG
     let writer = System.IO.File.WriteAllLines(@".\vocabularyBook.csv", text)
-    //
+    #else
+    let writer = System.IO.File.WriteAllLines(args.OutputDir, text)
+    #endif
     0 // return an integer exit code
